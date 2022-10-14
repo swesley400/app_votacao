34c48ee5412cbf281f8908b4f65bcf71647a7133
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-10-13 22:29:50 -0300

    adicao de modulos de votacao

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const porta = 3000
 
 const candidatos = require('./src/routes/candidatos')
-const votar = require('./src/routes/vota')
+const votar = require('./src/routes/votar.js')
 app.use(express.json())
 app.use(candidatos)
 app.use(votar)
