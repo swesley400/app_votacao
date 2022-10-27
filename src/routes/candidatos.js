8113c914d693c39505496ee9040af2c13ba7c034
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-10-26 22:19:21 -0300

    Update candidatos.js

--- src/routes/candidatos.js
+++ src/routes/candidatos.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const canditatosVindoDoBanco = require("../models/lerDadosCandidatosDoBanco")
 
 router.get('', async (req, res)=>{
-    res.send("Teste de Aplicacao")
     const candidatos =  await canditatosVindoDoBanco()
     res.json(candidatos)    
 })
