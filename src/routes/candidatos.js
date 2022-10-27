aaf0617eabfaefd3c1c645113677f5c64869c355
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-10-26 22:10:54 -0300

    teste

--- src/routes/candidatos.js
+++ src/routes/candidatos.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const canditatosVindoDoBanco = require("../models/lerDadosCandidatosDoBanco")
 
 router.get('', async (req, res)=>{
+    res.send("Teste de Aplicacao")
     const candidatos =  await canditatosVindoDoBanco()
     res.json(candidatos)    
 })
